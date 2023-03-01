61ff0e108e19c8a953fc2c694df7865d9f3279b9
Kwandapchumba <kwandapchumba@gmail.com>
2023-03-01 07:08:12 +0300

    modified config.env file

--- util/config.go
+++ util/config.go
@@ -23,8 +23,8 @@ type Config struct {
 
 func LoadConfig(path string) (config Config, err error) {
 	viper.AddConfigPath(path) // <- to work with Dockerfile setup
-	viper.SetConfigName("")
-	viper.SetConfigType("env")
+	// viper.SetConfigName("")
+	// viper.SetConfigType("env")
 	viper.SetConfigFile(".env")
 	viper.AutomaticEnv()
 
